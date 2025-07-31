317366cb784918a61447e771ac5de394a2f04bd5
mydevashish <devashishdas006@gmail.com>
2025-07-31 21:02:44 +0530

    [ADD]: Raise exception on negative numbers

--- incubyte-project.py
+++ incubyte-project.py
@@ -1,3 +1,5 @@
+import pytest
+
 def add(numbers: str) -> int:
     if not numbers:
         return 0
@@ -11,9 +13,10 @@ def add(numbers: str) -> int:
     parts = numbers.split(delimiter)
     return sum(int(p) for p in parts)
 
-def test_custom_delimiter_semicolon():
-    print(add("//;\n1;2") == 3)
-    assert add("//;\n1;2") == 3
+def test_negative_number_raises_exception():
+    with pytest.raises(Exception) as exc:
+        add("1,-2,3")
+    assert str(exc.value) == "negative numbers not allowed -2"
 
 if __name__ == "__main__":
-    test_custom_delimiter_semicolon()
+    test_negative_number_raises_exception()
