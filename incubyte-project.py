27922de0a07ca0345d32f8673fa4c56d3c944587
mydevashish <devashishdas006@gmail.com>
2025-07-31 20:30:52 +0530

    [FIX]: Add code to pass the test

--- incubyte-project.py
+++ incubyte-project.py
@@ -1,2 +1,10 @@
+def add(numbers: str) -> int:
+    if numbers == "":
+        return 0
+    return int(numbers)
+
 def test_single_number():
     assert add("1") == 1
+
+if __name__ == "__main__":
+    test_single_number()
