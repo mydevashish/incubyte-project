a3e04eaa24e3dd23926a3a9013ac65a532f13cfd
mydevashish <devashishdas006@gmail.com>
2025-07-31 20:56:26 +0530

    [FIX]: Handle new lines between numbers

--- incubyte-project.py
+++ incubyte-project.py
@@ -1,6 +1,7 @@
 def add(numbers: str) -> int:
     if not numbers:
         return 0
+    numbers = numbers.replace("\n", ",")
     parts = numbers.split(",")
     return sum(int(p) for p in parts)
 
