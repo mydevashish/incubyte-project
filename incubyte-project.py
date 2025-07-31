f7930519f485711dde7fb63ffa7bf09bebc10bae
mydevashish <devashishdas006@gmail.com>
2025-07-31 20:35:48 +0530

    [ADD]:Next test: two numbers

--- incubyte-project.py
+++ incubyte-project.py
@@ -3,8 +3,9 @@ def add(numbers: str) -> int:
         return 0
     return int(numbers)
 
-def test_single_number():
-    assert add("1") == 1
+def test_two_numbers():
+    print(add("1,2") == 3)
+    assert add("1,2") == 3
 
 if __name__ == "__main__":
-    test_single_number()
+    test_two_numbers()
