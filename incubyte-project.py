afc20e6443d06b1372fd9847f07cb8f6ed7d4687
mydevashish <devashishdas006@gmail.com>
2025-07-31 20:51:13 +0530

    [ADD]: Handle multiple numbers

--- incubyte-project.py
+++ incubyte-project.py
@@ -4,9 +4,9 @@ def add(numbers: str) -> int:
     parts = numbers.split(",")
     return sum(int(p) for p in parts)
 
-def test_two_numbers():
-    print(add("1,2") == 3)
-    assert add("1,2") == 3
+def test_multiple_numbers_return_sum():
+    print(add("1,2,3,4") == 10)
+    assert add("1,2,3,4") == 10
 
 if __name__ == "__main__":
-    test_two_numbers()
+    test_multiple_numbers_return_sum()
